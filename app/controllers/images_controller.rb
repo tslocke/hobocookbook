ea3e8daa63b613b820c5b2ed3f24e74554b9e9f2
Tom Locke <tom@tomlocke.com>
2008-11-01 10:22:01 +0000

    Source tidy

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -5,6 +5,8 @@ class ImagesController < ApplicationController
   auto_actions :create, :destroy
   
   def create
+    # The iframe based image uploader is not recognised as an ajax request,
+    # so we have to manually force the ajax response
     hobo_create { hobo_ajax_response }
   end
   
