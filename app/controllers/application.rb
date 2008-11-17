600a715decc91ef25ed009a5f3dca0c7e042da68
Tom Locke <tom@tomlocke.com>
2008-11-17 10:32:12 +0000

    Require login for all edit and new actions

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,12 +4,8 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'fa42ca8790f4b08ffbaf2b0b854bdbcc'
   
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  before_filter :login_required, :only => [:new, :edit]
+
 end
