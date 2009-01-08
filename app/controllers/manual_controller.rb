8f2d1b16544600756d8868d4e513328cad3f24ed
Tom Locke <tom@tomlocke.com>
2009-01-08 16:50:26 +0000

    Adding Controllers and Routing manual chapter (draft)

--- app/controllers/manual_controller.rb
+++ app/controllers/manual_controller.rb
@@ -2,11 +2,12 @@ class ManualController < ApplicationController
   
   caches_page :index, :manual_section
   
-  TITLES = ActiveSupport::OrderedHash.new [['to-do',       "To Do List"],
-                                           ['dryml-guide', "The DRYML Guide"],
-                                           ['permissions', "The Permission System"],
-                                           ['lifecycles',  'Lifecycles'],
-                                           ['viewhints',   'View Hints']]
+  TITLES = ActiveSupport::OrderedHash.new [['to-do',        "To Do List"],
+                                           ['dryml-guide',  "The DRYML Guide"],
+                                           ['permissions',  "The Permission System"],
+                                           ['controllers'], "Controllers and Routing"],
+                                           ['lifecycles',   'Lifecycles'],
+                                           ['viewhints',    'View Hints']]
   
   def manual_section
     section      = params[:section].gsub('[^a-z_]', '')
