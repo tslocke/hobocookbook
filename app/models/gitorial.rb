c2070dfaf938adf25c6bf487e79a06761d2a6195
Bryan Larsen <bryan@larsen.st>
2009-05-07 10:05:17 -0400

    update gitorial code

--- app/models/gitorial.rb
+++ app/models/gitorial.rb
@@ -1,18 +1,21 @@
 class Gitorial
   # initialize with `git log -p --reverse`, `.git/packed-refs`
-  def initialize(gitlogp, commit_link_base="http://github.com/bryanlarsen/agility-gitorial/commit/", tag_refs="")
+  def initialize(gitlogp, commit_link_base="http://github.com/bryanlarsen/agility-gitorial/commit/", patch_link_base="/patches/agility/", tag_refs="")
     @gitlogp=gitlogp
     @commit_link_base=commit_link_base
     @tag_refs=tag_refs
+    @patch_link_base=patch_link_base
   end
 
   def tags
     @tags ||= begin
                 tags = Hash.new { |hash, key| hash[key]=key.slice(0,6) }
-                @tag_refs.slice(1..-1).each {|ref|
+                @tag_refs.slice(1..-1).each do |ref|
                   refs = ref.split
-                  tags[refs[0]] = File.basename(refs[1])
-                }
+                  if File.dirname(refs[1])=="refs/tags"
+                    tags[refs[0]] = File.basename(refs[1])
+                  end
+                end
                 tags
               end
   end
@@ -30,15 +33,18 @@ class Gitorial
         patch << "    #{line}" if state==:patch
       elsif words[0]=="commit" or words[0]=="DONE"
         if !commit.nil?
-          # slice to remove first three line of message, which is the short message
-          markdown += message.slice(3..-1).map {|l|
+          # replace the short description line with a named link
+          shortlog = message[2]
+          message[2] = "<a name='#{shortlog}'> </a>"
+          markdown += message.map {|l|
             if l=="SHOW_PATCH"
               (patch+["{: .diff}\n"]).join("\n")
             else
               l
             end
           }
-          markdown << "\n[#{tags[commit]}](#{@commit_link_base}#{commit})\n{: .commit}\n"
+          series = tags[commit].slice(-2..-1)
+          markdown << "\n#{tags[commit]}: [view on github](#{@commit_link_base}#{commit}), [download #{series}-#{shortlog}.patch](#{@patch_link_base}/#{series}-#{shortlog}.patch)\n{: .commit}\n"
         end
         
         message=["\n"]
