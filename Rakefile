9792025d92b2cd13dccfc2e615654c4e163598b9
Bryan Larsen <bryan@larsen.st>
2009-05-13 17:15:33 -0400

    Testing out vlad.

--- Rakefile
+++ Rakefile
@@ -8,3 +8,6 @@ require 'rake/testtask'
 require 'rake/rdoctask'
 
 require 'tasks/rails'
+
+require 'vlad'
+Vlad.load(:app => :passenger, :scm => :git)
---
+++ config/deploy.rb
@@ -0,0 +1,4 @@
+set :application, "cookbook"
+set :domain, "cookbook-staging.hobocentral.net"
+set :deploy_to, "/home/newcookbook"
+set :repository, "git://github.com/tablatom/hobocookbook"
