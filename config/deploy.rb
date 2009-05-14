675eeac35d8f3b7bcde467c9e9ae3693543b768b
Bryan Larsen <bryan@larsen.st>
2009-05-14 12:39:04 -0400

    make generate_taglibs a separate vlad task

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, "cookbook"
 set :domain, "cookbook-staging.hobocentral.net"
 set :deploy_to, "/home/newcookbook"
-set :repository, "git://github.com/tablatom/hobocookbook"
+set :repository, "git://github.com/bryanlarsen/hobocookbook"
 set :revision, "origin/master"
 
 set :user, "cookbook"
@@ -13,9 +13,12 @@ namespace :vlad do
     run "touch #{current_release}/tmp/restart.txt"
   end
 
+  remote_task :generate_taglibs do
+    run "cd #{current_release}; RAILS_ENV=production rake hobo:generate_taglibs"
+  end
+
   desc 'reload api tags'
   remote_task :update_cookbook do
     run "cd #{current_release}; RAILS_ENV=production rake cookbook:load_api_docs"
-    run "cd #{current_release}; RAILS_ENV=production rake hobo:generate_taglibs"
   end
 end
