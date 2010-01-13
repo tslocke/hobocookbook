cd8bb75a492dabf43723568fdf01162a9cda3e31
Bryan Larsen <bryan@larsen.st>
2010-01-13 09:59:09 -0500

    steal & fix the underscore fix from github flavored markdown

--- app/rich_types/optional_markdown.rb
+++ app/rich_types/optional_markdown.rb
@@ -1,5 +1,30 @@
+class HoboFields::MarkdownString < HoboFields::RawMarkdownString
+
+  def to_html(xmldoctype = true)
+    return "" if blank?
+    # the underscore fix, stolen from GitHub Flavored Markdown
+    # which was buggy as all hell, so I had to fix it.   grrr.
+    s=self.gsub(/^(?! {4}|\t).*$/) do |line|
+      line.gsub(/\w+_\w+_\w[\w_]*/) do |x|
+        x.gsub('_', '\_') if x.split('').sort.to_s[0..1] == '__'
+      end
+    end
+    begin
+      s=Maruku.new(s).to_html
+    rescue
+      "Uh oh - looks like there was a problem rendering that markdown."
+    end
+    HoboFields::SanitizeHtml.sanitize(s)
+  end
+  
+end
+
 class OptionalMarkdown < HoboFields::Text
 
   HoboFields.register_type(:optional_markdown, self)
+
+  def to_html_from_markdown
+    HoboFields::MarkdownString.new(self).to_html
+  end
 
 end
