c3cbe8105aa26db8cc2226c8da20eab7e5d3f0a4
Bryan Larsen <bryan@larsen.st>
2012-03-22 14:27:19 -0400

    need longer token to run in dev mode

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Hobocookbook::Application.config.secret_token = 'REPLACE_ME_WITH_A_REAL_SECRET'
+Hobocookbook::Application.config.secret_token = 'REPLACE_ME_WITH_A_REAL_SECRET_BLAH_BLAH_BLAH_BLAH_BLAH_BLAH'
